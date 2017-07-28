28fc43886a1666d28982e090166e5311579b6209
Wikijito7 <wikyfg@gmail.com>
2017-07-28 16:31:55 +0200

    RG: Arreglado el comando "PointSet" y la generación del archivo "config.yml"

--- PA-Rage/src/es/projectalpha/pa/rage/cmd/PointSetCMD.java
+++ PA-Rage/src/es/projectalpha/pa/rage/cmd/PointSetCMD.java
@@ -3,7 +3,8 @@ package es.projectalpha.pa.rage.cmd;
 import es.projectalpha.pa.core.api.PAUser;
 import es.projectalpha.pa.core.cmd.PACmd;
 import es.projectalpha.pa.core.utils.Utils;
-import es.projectalpha.pa.rage.RageGames;
+import es.projectalpha.pa.rage.files.Files;
+
 
 import java.util.Arrays;
 
@@ -13,21 +14,15 @@ public class PointSetCMD extends PACmd {
         super("pointset", Grupo.Admin, Arrays.asList("ponerpunto", "ps"));
     }
 
-    private RageGames main = RageGames.getInstance();
-    int p = 0;
     @Override
     public void run(PAUser user, String label, String[] args) {
         if (args.length == 0) {
-            /*int p = this.plugin.getFiles().getConfig().getInt("puntos") + 1;
-            this.plugin.getFiles().getConfig().set("puntos", p);
-            this.plugin.getFiles().getConfig().set("spawns." + p, Utils.locationToString(user.getLoc()));
-            this.plugin.getFiles().saveFiles();*/
+            int p = Files.config.getInt("puntos");
             p++;
-            main.getFiles().getConfig().set("puntos", p);
-            main.getFiles().getConfig().set("spawns." + p, Utils.locationToString(user.getLoc()));
+            Files.config.set("puntos", p);
+            Files.config.set("spawns." + p, Utils.locationToString(user.getLoc()));
             user.sendMessage("&aPunto puesto satisfactoriamente " + "&6" + p);
-            main.getFiles().saveFiles();
-            System.out.println(main.getFiles().getConfig().getString("puntos" + " " + p));
+            Files.saveFiles();
         }
     }
 }
--- PA-Rage/src/es/projectalpha/pa/rage/files/Files.java
+++ PA-Rage/src/es/projectalpha/pa/rage/files/Files.java
@@ -10,27 +10,21 @@ import java.io.IOException;
 
 public class Files {
 
-    @Getter private File fileConfig = new File("plugins/PA-Rage/", "config.yml");
-    private YamlConfiguration config = YamlConfiguration.loadConfiguration(fileConfig);
+    @Getter public static File fileConfig = new File("plugins/PA-Rage/", "config.yml");
+    public static YamlConfiguration config = YamlConfiguration.loadConfiguration(fileConfig);
 
     public void setupFiles() {
-        System.out.println("SetupFiles antes de detectar fileConfig");
         if (!fileConfig.exists()) {
-            System.out.println("SetupFiles generandose");
             fileConfig.mkdir();
-            //config.set("puntos", 0);
+            config.set("puntos", 0);
         }
-        System.out.println("SetupFiles detectado, no se regenera");
         saveFiles();
-        System.out.println("Archivo guardado y cargados, setupFiles finalizado.");
     }
 
-    public void saveFiles() {
+    public static void saveFiles() {
         try {
             config.save(fileConfig);
-            System.out.println("Guardando archivos");
             config.load(fileConfig);
-            System.out.println("Cargando archivos");
         } catch (IOException | InvalidConfigurationException e) {
             e.printStackTrace();
         }
