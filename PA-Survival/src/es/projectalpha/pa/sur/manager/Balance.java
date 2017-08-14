c77eddb41599e245497ca6aa0ba48f51c61baf32
Cristian <wikyfg@gmail.com>
2017-08-14 20:16:05 +0200

    Survival: Añadido el sistema de impuestos y tocado la clase Balance.

--- PA-Survival/src/es/projectalpha/pa/sur/manager/Balance.java
+++ PA-Survival/src/es/projectalpha/pa/sur/manager/Balance.java
@@ -5,26 +5,28 @@ import es.projectalpha.pa.sur.PASurvival;
 import es.projectalpha.pa.sur.api.SurvivalUser;
 import net.milkbowl.vault.economy.Economy;
 
-public class Balance {
+public final class Balance {
     private Economy eco;
     private Files files;
 
     public void saveBalance(SurvivalUser p){
+        if(files.getUser().getInt("Users." + p.getName() + ".money") == eco.getBalance(p.getPlayer())) return;
         files.getUser().set("Users." + p.getName() + ".money", eco.getBalance(p.getPlayer()));
     }
 
-    public void addBalace(SurvivalUser p, int v){
+    public void addBalace(SurvivalUser p, double v){
         if(files.getUser().getInt("Users." + p.getName() + ".money") != eco.getBalance(p.getPlayer())) saveBalance(PASurvival.getPlayer(p.getPlayer()));
         files.getUser().set("Users." + p.getName() + ".money", files.getUser().getInt("Users." + p.getName() + ".money") + v);
     }
 
-    public void removeBalance(SurvivalUser p, int v){
+    public void removeBalance(SurvivalUser p, double v){
         if(files.getUser().getInt("Users." + p.getName() + ".money") != eco.getBalance(p.getPlayer())) saveBalance(PASurvival.getPlayer(p.getPlayer()));
         files.getUser().set("Users." + p.getName() + ".money", files.getUser().getInt("Users." + p.getName() + ".money") - v);
     }
 
     public void resetBalance(SurvivalUser p){
-        files.getUser().set("Users." + p.getName() + ".money",0);
+        eco.withdrawPlayer(p.getPlayer(), eco.getBalance(p.getPlayer()));
+        saveBalance(p);
     }
 
 }
--- PA-Survival/src/es/projectalpha/pa/sur/tasks/TimeTask.java
+++ PA-Survival/src/es/projectalpha/pa/sur/tasks/TimeTask.java
@@ -1,5 +1,12 @@
 package es.projectalpha.pa.sur.tasks;
 
+import es.projectalpha.pa.sur.PASurvival;
+import es.projectalpha.pa.sur.files.Files;
+import es.projectalpha.pa.sur.manager.Balance;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.text.DateFormat;
@@ -8,15 +15,29 @@ import java.util.*;
 
 public class TimeTask extends BukkitRunnable {
 
-    Calendar cal = Calendar.getInstance();
-    int hora,horaf;
-    SimpleDateFormat sdf = new SimpleDateFormat("HH");
+    private Calendar cal = Calendar.getInstance();
+    private int hora,min,seg;
+    private Files files = new Files();
+    private Economy eco;
+    private PASurvival plugin;
+    private Balance balance;
 
     public void run() {
         cal = new GregorianCalendar();
         hora = cal.get(Calendar.HOUR_OF_DAY);
-        if(hora != 6) return;
-
+        min = cal.get(Calendar.MINUTE);
+        seg = cal.get(Calendar.SECOND);
+        Bukkit.getOnlinePlayers().forEach(p -> balance.saveBalance(PASurvival.getPlayer(p)));
+        if(hora != 6 && min !=0 && seg != 0) return;
+        System.out.println(hora + ":" + min + ":" + seg);
+        files.getUser().getList("Users.").forEach(p ->{
+                balance.saveBalance(PASurvival.getPlayer(plugin.getServer().getOfflinePlayer(p.toString())));
+                eco.withdrawPlayer(plugin.getServer().getPlayer(p.toString()), eco.getBalance(plugin.getServer().getPlayer(p.toString())));
+                balance.removeBalance(PASurvival.getPlayer(plugin.getServer().getOfflinePlayer(p.toString())), eco.getBalance(plugin.getServer().getPlayer(p.toString())) * 0.01);
+                balance.saveBalance(PASurvival.getPlayer(plugin.getServer().getOfflinePlayer(p.toString())));
+                eco.depositPlayer(plugin.getServer().getPlayer(p.toString()), files.getUser().getInt("Users." + plugin.getServer().getPlayer(p.toString() + ".money")));
+        });
+        Bukkit.getOnlinePlayers().forEach(u -> u.sendMessage(ChatColor.GREEN + "Hora de los impuestos, se te ha quitado 1% de tu dinero, o lo que es lo mismo " + eco.getBalance(plugin.getServer().getPlayer(u.toString())) * 0.01));
     }
 
 
