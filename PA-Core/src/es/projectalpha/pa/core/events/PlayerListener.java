746e3d116716e17c3f56191afc9decd880737a78
Iván <cadox8@gmail.com>
2017-08-24 15:45:12 +0200

    Core: Fixed chat

--- PA-Core/src/es/projectalpha/pa/core/events/PlayerListener.java
+++ PA-Core/src/es/projectalpha/pa/core/events/PlayerListener.java
@@ -79,9 +79,9 @@ public class PlayerListener implements Listener {
         }
 
         //Format
-        String tag = "[&" + PACmd.Grupo.groupColor(user.getUserData().getGrupo()) + WordUtils.capitalizeFully(user.getUserData().getGrupo().toString().toLowerCase()) + "&r] " + PACmd.Grupo.groupColor(user.getUserData().getGrupo()) + user.getName() + "&r:";
+        String tag = "[&" + PACmd.Grupo.groupColor(user.getUserData().getGrupo()) + WordUtils.capitalizeFully(user.getUserData().getGrupo().toString().toLowerCase()) + "&r] &" + PACmd.Grupo.groupColor(user.getUserData().getGrupo()) + user.getName() + "&r: ";
         if (user.isOnRank(PACmd.Grupo.ORIGIN)) e.setMessage(Utils.colorize(e.getMessage()));
-        e.setFormat(Utils.colorize(tag) + "%s");
+        e.setFormat(Utils.colorize(tag) + e.getMessage());
     }
 
     /*
