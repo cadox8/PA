ccf214c6bc86e934a3131ecf6f0fd7c2f3197382
Iván <cadox8@gmail.com>
2017-09-14 18:58:26 +0200

    Survival: fixed bug

--- PA-Survival/src/es/projectalpha/pa/sur/api/SurvivalUser.java
+++ PA-Survival/src/es/projectalpha/pa/sur/api/SurvivalUser.java
@@ -7,6 +7,8 @@ import es.projectalpha.pa.core.utils.ScoreboardUtil;
 import es.projectalpha.pa.sur.PASurvival;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.text.DecimalFormat;
+
 public class SurvivalUser extends PAUser {
 
     private PASurvival plugin = PASurvival.getInstance();
@@ -16,7 +18,7 @@ public class SurvivalUser extends PAUser {
     }
 
     public void setInfo() {
-        sendToLobby();
+        DecimalFormat df = new DecimalFormat("#");
         ScoreboardUtil board = new ScoreboardUtil(PAData.SURVIVAL.getOldPrefix(), "sur");
         new BukkitRunnable() {
             @Override
@@ -28,7 +30,7 @@ public class SurvivalUser extends PAUser {
                 board.text(5, "§aMundo: §e" + getLoc().getWorld().getName());
                 board.text(4, "§6 ");
                 board.text(3, "§aCoordenadas:");
-                board.text(2, "§dX: §e" + getLoc().getX() + " §dY: §e" + getLoc().getY() + " §dZ: §e" + getLoc().getZ());
+                board.text(2, "§dX: §e" + Double.valueOf(df.format(getLoc().getX())) + " §dY: §e" + Double.valueOf(df.format(getLoc().getY())) + " §dZ: §e" + Double.valueOf(df.format(getLoc().getZ())));
                 board.text(1, "§e ");
                 board.text(0, PACore.getOLD_IP());
                 if (getPlayer() != null) board.build(getPlayer());
